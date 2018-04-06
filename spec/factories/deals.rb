ca0ef75c665eba2432b56886eb3c575277372deb
Parth Modi <parthmodi54@yahoo.com>
2018-04-06 23:37:33 +0530

    deals, users factorybot added

--- spec/factories/deals.rb
+++ spec/factories/deals.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :deal do
-    
+    status :pending
+    title 'Winning Deal'
+    value 50000
+    customer { Faker::Name.name }
+    created_by_id 1
+    user_id 1
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name "John"
     last_name  "Doe"
-    email { "some#{Random.rand(1..100)}@gmail.com" }    
+    email { "some#{Random.rand(1..1000)}@gmail.com" }
+    password 'password'
   end
 end
