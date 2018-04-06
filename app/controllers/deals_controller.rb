1d4ec1b501642aa6629d851e664db8c472f432ef
Parth Modi <parthmodi54@yahoo.com>
2018-04-06 23:37:14 +0530

    default sorting for deals added

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -76,6 +76,7 @@ class DealsController < ApplicationController
 
   def fetch_deals
     @q = Deal.by_user_id(current_user.id).ransack(params[:q])
+    @q.sorts = 'created_at desc' if @q.sorts.empty?
     @deals = @q.result(distinct: true).page(params[:page])
   end
 
