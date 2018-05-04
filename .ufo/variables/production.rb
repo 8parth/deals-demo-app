38ffa01da924332fc483c688dfab20ff2f7b481e
Parth Modi <parthmodi54@yahoo.com>
2018-05-04 09:28:53 +0530

    .env file used to load env vars

--- .ufo/variables/production.rb
+++ .ufo/variables/production.rb
@@ -1,7 +1,4 @@
 # Example ufo/variables/production.rb
 # More info on how variables work: http://ufoships.com/docs/variables/
 @cpu = 256
-@environment = helper.env_vars(%Q[
-  RAILS_ENV=production
-  SECRET_KEY_BASE=secret
-])
+@environment = helper.env_file('.env')
